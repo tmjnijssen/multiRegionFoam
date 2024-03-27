b7d59fd7caf9d2c11dfd8fe9119b0b4e86ca6b26
Sebastiaan Kuipers <s.q.h.kuipers@tudelft.nl>
2024-03-27 14:24:22 +0100

    [FIX] IcoCase fix

--- src/regions/transportTemperature/transportTemperature.C
+++ src/regions/transportTemperature/transportTemperature.C
@@ -71,18 +71,17 @@ Foam::regionTypes::transportTemperature::transportTemperature
     ),
     k_(transportProperties_.lookup("k")),
     cp_(transportProperties_.lookup("cp")),
+    rho_(transportProperties_.lookup("rho")),
 
     U_(nullptr),
     kappa_(nullptr),
     phi_(nullptr),
-    rho_(nullptr),
     T_(nullptr)
 {
     // set velocity field
     // Postponing field creation since U is probably provided by
     // another regionType, e.g. icoFluid, and thus to be re-used.
     U_ = lookupOrRead<volVectorField>(mesh(), "U");
-    rho_ = lookupOrRead<volScalarField>(mesh(), "rho");
 
     // set flux field
     phi_ = lookupOrRead<surfaceScalarField>
@@ -91,7 +90,7 @@ Foam::regionTypes::transportTemperature::transportTemperature
         "phi",
         false,
         true,
-        linearInterpolate(rho_()*U_()) & mesh().Sf()
+        linearInterpolate(U_()) & mesh().Sf()
     );
 
     // set thermal conductivity field
@@ -131,9 +130,9 @@ void Foam::regionTypes::transportTemperature::setCoupledEqns()
 {
     TEqn =
     (
-        cp_
+        rho_*cp_
        *(
-            fvm::ddt(T(), rho_())
+            fvm::ddt(T())
           + fvm::div(phi_(), T())
         )
      ==
@@ -181,3 +180,4 @@ void Foam::regionTypes::transportTemperature::meshMotionCorrector()
 }
 
 // ************************************************************************* //
+
