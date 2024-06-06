9ecf615d38c1473e4f3ce2329752140cca6c36b9
Tim MJ Nijssen <t.m.j.nijssen@tudelft.nl>
2024-06-06 12:35:49 +0200

    [ADD] reactingFluid: initial commit

---
+++ src/regions/reactingFluid/reactingFluid.C
@@ -0,0 +1,531 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "label.H"
+#include "reactingFluid.H"
+#include "fvCFD.H"
+#include "basicPsiThermo.H"
+#include "zeroGradientFvPatchFields.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace regionTypes
+{
+    defineTypeNameAndDebug(reactingFluid, 0);
+
+    addToRunTimeSelectionTable
+    (
+        regionType,
+        reactingFluid,
+        dictionary
+    );
+}
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::regionTypes::reactingFluid::reactingFluid
+(
+    const Time& runTime,
+    const word& regionName
+)
+:
+    regionType(runTime, regionName),
+
+    regionName_(regionName),
+
+    pimple_(mesh()),
+
+    pThermo_(basicPsiThermo::New(mesh())),
+
+    p_(nullptr),
+    h_(nullptr),
+    psi_(nullptr),
+    rho_(nullptr),
+    U_(nullptr),
+    phi_(nullptr),
+    turbulence_(nullptr),
+    T_(nullptr),
+    sigma_(nullptr),
+    kappa_(nullptr),
+    kappaEff_(nullptr),
+    Cp_(nullptr),
+
+    DpDt_(nullptr),
+
+    pMin_(pimple_.dict().lookup("pMin")),
+
+    mrfZones_(mesh()),
+    myTimeIndex_(mesh().time().timeIndex()),
+
+    maxCo_
+    (
+        mesh().time().controlDict().lookupOrDefault<scalar>("maxCo", 1.0)
+    ),
+    maxDeltaT_
+    (
+        mesh().time().controlDict().lookupOrDefault<scalar>("maxDeltaT", GREAT)
+    ),
+
+    correctPhi_
+    (
+        pimple_.dict().lookupOrDefault<Switch>("correctPhi", false)
+    ),
+    checkMeshCourantNo_
+    (
+        pimple_.dict().lookupOrDefault<Switch>("checkMeshCourantNo", false)
+    ),
+
+    sumLocalContErr_(0),
+    globalContErr_(0),
+    cumulativeContErr_(0)
+{
+    p_ = lookupOrRead<volScalarField>
+    (
+        mesh(),
+        "p",
+        true,
+        true
+    );
+
+    h_ = lookupOrRead<volScalarField>
+    (
+        mesh(),
+        "h",
+        true,
+        true
+    );
+
+    psi_ = lookupOrRead<volScalarField>
+    (
+        mesh(),
+        "psi",
+        true,
+        true
+    );
+
+    rho_ = lookupOrRead<volScalarField>
+    (
+        mesh(),
+        "rho",
+        false,
+        true,
+        pThermo_().rho()
+    );
+    
+
+    U_ = lookupOrRead<volVectorField>
+    (
+        mesh(),
+        "U",
+        true,
+        true
+    );
+
+    phi_ = lookupOrRead<surfaceScalarField>
+    (
+        mesh(),
+        "phi",
+        false,
+        true,
+        linearInterpolate(rho_()*U_()) & mesh().Sf()
+    );
+
+    turbulence_ = compressible::turbulenceModel::New
+    (
+        rho_(),
+        U_(),
+        phi_(),
+        pThermo_()
+    );
+
+    T_ = lookupOrRead<volScalarField>
+    (
+        mesh(),
+        "T",
+        true,
+        true
+    );
+
+    sigma_ = lookupOrRead<volSymmTensorField>
+    (
+        mesh(),
+        "sigma",
+        false,
+        true,
+        -p_()*symmTensor(1,0,0,1,0,1) - turbulence_().devRhoReff()
+    );   
+
+    kappa_ = lookupOrRead<volScalarField>
+    (
+        mesh(),
+        "kappa",
+        false,
+        true,
+        pThermo_().alpha() * pThermo_().Cp()
+    );   
+
+    kappaEff_ = lookupOrRead<volScalarField>
+    (
+        mesh(),
+        "kappaEff",
+        false,
+        true,
+        turbulence_().alphaEff() * pThermo_().Cp()
+    );
+
+    Cp_ = lookupOrRead<volScalarField>
+    (
+        mesh(),
+        "Cp",
+        false,
+        true,
+        kappa_() / pThermo_().alpha()
+    );
+
+    DpDt_ = lookupOrRead<volScalarField>
+    (
+        mesh(),
+        "DpDt",
+        false,
+        true,
+        fvc::DDt
+        (
+            surfaceScalarField("phiU", phi_()/fvc::interpolate(rho_())),
+            p_()
+        )
+    );
+
+    // phid_ = lookupOrRead<surfaceScalarField>
+    // (
+    //     mesh(),
+    //     "phid",
+    //     false,
+    //     true,
+    //     fvc::interpolate(psi)
+    //    *(
+    //         (fvc::interpolate(U_()) & mesh.Sf())
+    //       + fvc::ddtPhiCorr(rAU, rho, U, phi)
+    //     )
+    // );
+
+    mesh().schemesDict().setFluxRequired(p_().name());
+}
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::regionTypes::reactingFluid::~reactingFluid()
+{}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::regionTypes::reactingFluid::correct()
+{
+#       include "reactingFluidCourantNo.H"
+}
+
+
+Foam::scalar Foam::regionTypes::reactingFluid::getMinDeltaT()
+{
+    //- TODO: implement deltaT based on CFL criteria
+    return GREAT;
+}
+
+
+void Foam::regionTypes::reactingFluid::setCoupledEqns()
+{
+
+}
+
+
+void Foam::regionTypes::reactingFluid::postSolve()
+{
+    if (myTimeIndex_ < mesh().time().timeIndex())
+    {
+        mrfZones_.translationalMRFs().correctMRF();
+
+        mrfZones_.translationalMRFs().correctBoundaryVelocity(U_(), phi_());
+
+        myTimeIndex_ = mesh().time().timeIndex();
+    }
+}
+
+
+void Foam::regionTypes::reactingFluid::solveRegion()
+{
+    // Solve the continuity for density.
+    solve(fvm::ddt(rho_()) + fvc::div(phi_()));
+}
+
+void Foam::regionTypes::reactingFluid::prePredictor()
+{
+    Info<< "Pre-predictor for " << this->typeName
+        << " in region " << mesh().name()
+        << endl;
+
+    loopPIMPLE_ = pimple_.loop();
+
+    if (!loopPIMPLE_)
+    {
+        Info<< "Breaking PIMPLE loop for " << this->typeName
+        << " in region " << mesh().name()
+        << endl;
+        return;
+    }
+}
+
+void Foam::regionTypes::reactingFluid::momentumPredictor()
+{
+    Info<< "Momentum predictor for " << this->typeName
+        << " in region " << mesh().name()
+        << endl;
+
+    // Convection-diffusion matrix
+    tUEqn =
+        (
+            fvm::ddt(rho_(), U_())
+          + fvm::div(phi_(), U_())
+          + turbulence_().divDevRhoReff()
+        );
+    fvVectorMatrix& UEqn = tUEqn();
+
+    mrfZones_.translationalMRFs().addFrameAcceleration(UEqn, rho_());
+
+    UEqn.relax
+    (
+        mesh().solutionDict().equationRelaxationFactor
+        (
+            U_().select(pimple_.finalIter())
+        )
+    );
+
+    volScalarField rAU = 1.0/UEqn.A();
+
+    if (pimple_.momentumPredictor())
+    {
+        solve
+        (
+            UEqn == -fvc::grad(p_()),
+            mesh().solutionDict().solver((U_().select(pimple_.finalIter())))
+        );
+    }
+    else
+    {
+        U_() = rAU*(UEqn.H() - fvc::grad(p_()));
+        U_().correctBoundaryConditions();
+    }
+
+    fvScalarMatrix hEqn
+    (
+        fvm::ddt(rho_(), h_())
+      + fvm::div(phi_(), h_())
+      - fvm::laplacian(turbulence_().alphaEff(), h_())
+     ==
+        DpDt_()
+    );
+
+    hEqn.relax
+    (
+        mesh().solutionDict().equationRelaxationFactor
+        (
+            h_().select(pimple_.finalIter())
+        )
+    );
+    hEqn.solve
+    (
+        mesh().solutionDict().solver((h_().select(pimple_.finalIter())))
+    );
+
+    pThermo_().correct();
+}
+
+void Foam::regionTypes::reactingFluid::pressureCorrector()
+{
+    Info<< "Pressure corrector for " << this->typeName
+        << " in region " << mesh().name()
+        << endl;
+
+    // Get cached matricies from momentum predictor
+    fvVectorMatrix& UEqn = tUEqn();
+
+    // --- PISO loop
+    while (pimple_.correct())
+    {
+        rho_() = pThermo_().rho();
+        volScalarField rAU = 1.0/UEqn.A();
+
+        // Calculate U from convection-diffusion matrix
+        U_() = rAU*UEqn.H();
+
+        if (pimple_.transonic())
+        {
+            surfaceScalarField phid
+            (
+                "phid",
+                fvc::interpolate(psi_())
+               *(
+                    (fvc::interpolate(U_()) & mesh().Sf())
+                  + fvc::ddtPhiCorr(rAU, rho_(), U_(), phi_())
+                )
+            );
+
+            while (pimple_.correctNonOrthogonal())
+            {
+                fvScalarMatrix pEqn
+                (
+                    fvm::ddt(psi_(), p_())
+                  + fvm::div(phid, p_())
+                  - fvm::laplacian(rho_()*rAU, p_())
+                );
+
+                pEqn.solve
+                (
+                    mesh().solutionDict().solver
+                    (
+                        p_().select(pimple_.finalInnerIter())
+                    )
+                );
+
+                if (pimple_.finalNonOrthogonalIter())
+                {
+                    phi_() == pEqn.flux();
+                }
+            }
+        }
+        else
+        {
+            phi_() =
+                fvc::interpolate(rho_())*
+                (
+                    (fvc::interpolate(U_()) & mesh().Sf())
+                );
+
+            while (pimple_.correctNonOrthogonal())
+            {
+                // Pressure corrector
+                fvScalarMatrix pEqn
+                (
+                    fvm::ddt(psi_(), p_())
+                  + fvc::div(phi_())
+                  - fvm::laplacian(rho_()*rAU, p_())
+                );
+
+                pEqn.solve
+                (
+                    mesh().solutionDict().solver
+                    (
+                        p_().select(pimple_.finalInnerIter())
+                    )
+                );
+
+                if (pimple_.finalNonOrthogonalIter())
+                {
+                    phi_() += pEqn.flux();
+                }
+            }
+        }
+
+        // Solve continuity for density
+        solve(fvm::ddt(rho_()) + fvc::div(phi_()));
+        #include "reactingFluidContinuityErrs.H"
+
+        {
+            // Explicitly relax pressure for momentum corrector
+            p_().relax();
+
+            rho_() = pThermo_().rho();
+            rho_().relax();
+            Info<< "  " << mesh().name() << ": rho min/mean/max: " 
+                << gMin(rho_()) << "/"
+                << gAverage(rho_()) << "/"
+                << gMax(rho_()) << endl;
+        }
+
+        U_() -= rAU*fvc::grad(p_());
+        U_().correctBoundaryConditions();
+
+        DpDt_() = fvc::DDt
+            (
+                surfaceScalarField("phiU", phi_()/fvc::interpolate(rho_())),
+                p_()
+            );
+
+        bound(p_(), pMin_);
+
+        // Update sigma field
+        sigma_() = -p_()*symmTensor(1,0,0,1,0,1) - turbulence_().devRhoReff();
+    }
+
+    turbulence_().correct();
+
+    // output fields
+    kappa_() = pThermo_().alpha() * pThermo_().Cp();
+    kappaEff_() = turbulence_().alphaEff() * pThermo_().Cp();
+    Cp_() = pThermo_().Cp();
+
+    Info<< "  " << mesh().name() << ": pressure min/mean/max: "
+        << gMin(p_()) << "/"
+        << gAverage(p_()) << "/"
+        << gMax(p_()) << nl
+        << "  " << mesh().name() << ": velocity min/mean/max: "
+        << gMin(U_()) << "/"
+        << gAverage(U_()) << "/"
+        << gMax(U_()) << nl
+        << "  " << mesh().name() << ": volume: "
+        << gSum(mesh().V()) << endl;
+}
+
+void Foam::regionTypes::reactingFluid::meshMotionCorrector()
+{
+//     // Make the fluxes absolute
+//     fvc::makeAbsolute(phi_(), U_());
+
+//     mesh().update();
+
+// #       include "reactingFluidVolContinuity.H"
+
+//     if (mesh().changing() && correctPhi_)
+//     {
+// #       include "reactingFluidCorrectPhi.H"
+//     }
+
+//     // Make the fluxes relative to the mesh motion
+//     fvc::makeRelative(phi_(), U_());
+
+//     if (mesh().moving() && checkMeshCourantNo_)
+//     {
+// #           include "reactingFluidMeshCourantNo.H"
+//     }
+
+//     if (mesh().changing())
+//     {
+// #           include "reactingFluidCourantNo.H"
+//     }
+}
+
+// ************************************************************************* //
