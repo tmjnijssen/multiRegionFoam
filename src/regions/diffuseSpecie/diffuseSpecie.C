8eb4ad693dc915d30c3a4aba6ced30d26abece0c
Sebastiaan Kuipers <s.q.h.kuipers@tudelft.nl>
2024-10-22 16:49:17 +0200

    [MOD] Explicit physisorption implementation

--- src/regions/diffuseSpecie/diffuseSpecie.C
+++ src/regions/diffuseSpecie/diffuseSpecie.C
@@ -332,12 +332,7 @@ void Foam::regionTypes::diffuseSpecie::setCoupledEqns(word fieldName)
             eps_*fvm::ddt(CO2_())
          ==
             fvm::laplacian(DporeCO2_, CO2_())
-          - fvm::SuSp
-            (
-                (1-eps_)/eps_*dqdtCO2im_, 
-                CO2_()
-            )
-          - ((1-eps_)/eps_) * dqdtCO2ex_ 
+            - ((1-eps_)/(eps_))*dqdtCO2_
         );
 
         fvScalarMatrices.set
@@ -361,13 +356,7 @@ void Foam::regionTypes::diffuseSpecie::setCoupledEqns(word fieldName)
             eps_*fvm::ddt(H2O_())
          ==
             fvm::laplacian(DporeH2O_, H2O_())
-          - fvm::SuSp
-            (
-                ((1-eps_)/eps_)*dqdtH2Oim_,
-                 H2O_()
-            )
-          - ((1-eps_)/eps_) * dqdtH2Oex_
-
+            - ((1-eps_)/(eps_))*dqdtH2O_
         );
 
         fvScalarMatrices.set
@@ -389,8 +378,8 @@ void Foam::regionTypes::diffuseSpecie::postSolve()
 void Foam::regionTypes::diffuseSpecie::solveRegion()
 {
     // physisorbed species
-    //volScalarField HCO2 = HrCO2_*CO2_();
-    //volScalarField HH2O = HrH2O_*H2O_();
+    volScalarField HCO2 = HrCO2_*CO2_();
+    volScalarField HH2O = HrH2O_*H2O_();
 
     dimensionedScalar R = dimensionedScalar("R", dimEnergy/dimMoles/dimTemperature, 8.314);
     volScalarField invRT = (1./(R*T_()));
@@ -411,36 +400,26 @@ void Foam::regionTypes::diffuseSpecie::solveRegion()
     volScalarField K3 = exp(-dG3 * invRT);
 
     // carbamate reaction rate
-    volScalarField R1ex = -(k1/K1) * R2NH2p_*R2NCO2m_*dimensionedScalar("one", dimMoles/dimVolume, 1.0);  // explicit part
-    volScalarField R1im = k1 * HrCO2_*R2NH_*R2NH_; // implicit part
-    volScalarField R1   = R1ex + R1im*CO2_();       // total carbamate rate
-    // bicarbonate reaction rate
-    volScalarField R2ex = -(k2/K2) * R2NH2p_*HCO3m_*dimensionedScalar("one", dimMoles/dimVolume, 1.0); // explicit part
-    volScalarField R2im = k2 * HrCO2_*HrH2O_*R2NH_;   // implicit part
-    volScalarField R2   = R2ex + (R2im*CO2_()*H2O_()); // total bicarbamate rate
-    // physical adsorption water
-    volScalarField R3ex = -(k3/K3) * H2O_()*H2O_();// explicit part
-    volScalarField R3im = k3 * HrH2O_*HrH2O_*H2O_(); // implicit part
-    volScalarField R3 = R3ex + R3im;                  // total water adsorption rate
+    volScalarField R1 = k1 * (HCO2*R2NH_*R2NH_ - (1./K1)*R2NH2p_*R2NCO2m_*dimensionedScalar("one", dimMoles/dimVolume, 1.0));
 
+    // bicarbonate reaction rate
+    volScalarField R2 = k2 * (HCO2*HH2O*R2NH_ - (1./K2)*R2NH2p_*HCO3m_*dimensionedScalar("one", dimMoles/dimVolume, 1.0));
+
+    // water physisorption rate
+    volScalarField R3 = (k3 * (H2O_() - (1./K3)*H2Oads_)*dimensionedScalar("one", dimMoles/dimVolume, 1.0))*dimensionedScalar("one", dimMoles/dimVolume, 1.0);
     // solve adsorbed species
     solve(fvm::ddt(R2NH_   ) == -2*R1 - R2);
     solve(fvm::ddt(R2NH2p_ ) ==    R1 + R2);
     solve(fvm::ddt(R2NCO2m_) ==    R1     );
     solve(fvm::ddt(HCO3m_  ) ==         R2);
-    solve(fvm::ddt(H2Oads_) ==  2*R3 - R2 );
+    solve(fvm::ddt(H2Oads_  ) ==  2*R3 - R2);
 
     // total adsorption rate
-    dqdtCO2im_ = R1im + R2im*H2O_();
-    dqdtCO2ex_ = R1ex + R2ex;
-    dqdtCO2_   = dqdtCO2im_*CO2_() + dqdtCO2ex_;
-
-    dqdtH2Oim_ = R2im*CO2_() + R3im;
-    dqdtH2Oex_ = R2ex + R3ex;
-    dqdtH2O_   = dqdtH2Oim_*H2O_() + dqdtH2Oex_;
+    dqdtCO2_ = R1 + R2;
+    dqdtH2O_ = R2 + R3;
 
     // heat source
-    heatSource_() = -(R1 * dH1_ + R2 * dH2_ + R3 * dH3_);
+    heatSource_() = -(R1 * dH1_ + R2 * dH2_);
 
    CO2_().correctBoundaryConditions();
    H2O_().correctBoundaryConditions();
