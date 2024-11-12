c1065ba5f846115835a49c9c4a0424493df3f0ce
Lucas Huefner <l.huefner@ianus-simulation.de>
2024-11-12 13:58:10 +0100

    [ADD] regionTypes for temperature and electron conduction (for tabs in NTGK)

---
+++ src/regions/conductNegPotentialTemperature/conductNegPotentialTemperature.C
@@ -0,0 +1,198 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "conductNegPotentialTemperature.H"
+#include "zeroGradientFvPatchFields.H"
+#include "addToRunTimeSelectionTable.H"
+#include "thermoPhysicalConstants.H"
+
+using namespace thermoPhysicalConstant;
+
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace regionTypes
+{
+    defineTypeNameAndDebug(conductNegPotentialTemperature, 0);
+
+    addToRunTimeSelectionTable
+    (
+        regionType,
+        conductNegPotentialTemperature,
+        dictionary
+    );
+}
+}
+// * * * * * * * * * * * * * * * Private Functions * * * * * * * * * * * * * //
+
+void Foam::regionTypes::conductNegPotentialTemperature::calculateJouleHeating()
+{    
+    
+    //volScalarField Qohm = sigmaNeg_*(fvc::grad(faiNeg_())&fvc::grad(faiNeg_()));
+    
+    //ST_() = Qohm;
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::regionTypes::conductNegPotentialTemperature::conductNegPotentialTemperature
+(
+    const Time& runTime,
+    const word& regionName
+)
+:
+    regionType(runTime, regionName),
+
+    regionName_(regionName),
+
+    transportProperties_
+    (
+        IOobject
+        (
+            "transportProperties",
+            mesh().time().constant(),
+            mesh(),
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        )
+    ),
+    
+    sigmaNeg_(transportProperties_.lookup("sigmaNeg")),
+    rho_(transportProperties_.lookup("rho")),
+    cp_(transportProperties_.lookup("cp")),
+    k_(transportProperties_.lookup("k")),
+    ST_(nullptr),
+    faiNeg_(nullptr),
+    T_(nullptr)
+{
+    
+    // set summarized heat source terms field
+    ST_ = lookupOrRead<volScalarField>
+    (
+        mesh(),
+        "ST",
+        dimensionedScalar("STinit", dimensionSet(1, -1, -3, 0, 0, 0, 0), 0),
+        true
+    );
+
+    // set negative electrode potential field
+    faiNeg_ = lookupOrRead<volScalarField>(mesh(), "faiNeg");
+    
+    // set temperature field
+    T_ = lookupOrRead<volScalarField>(mesh(), "T");
+
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::regionTypes::conductNegPotentialTemperature::~conductNegPotentialTemperature()
+{}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::regionTypes::conductNegPotentialTemperature::correct()
+{
+    
+}
+
+
+Foam::scalar Foam::regionTypes::conductNegPotentialTemperature::getMinDeltaT()
+{
+    return GREAT;
+}
+
+
+void Foam::regionTypes::conductNegPotentialTemperature::setCoupledEqns()
+{
+	calculateJouleHeating();
+         	
+	faiNegEqn =
+    (
+       - fvm::laplacian(sigmaNeg_, faiNeg(), "laplacian(sigma,fai)")
+    );
+
+    TEqn =
+    (
+         fvm::ddt(rho_*cp_, T())
+       - fvm::laplacian(k_, T(), "laplacian(k,T)")
+       ==
+         ST_()
+    );
+    
+    fvScalarMatrices.set
+    (
+        faiNeg_().name()
+      + mesh().name() + "Mesh"
+      + conductNegPotentialTemperature::typeName + "Type"
+      + "Eqn",
+        &faiNegEqn()
+    );
+
+    fvScalarMatrices.set
+    (
+        T_().name()
+      + mesh().name() + "Mesh"
+      + conductNegPotentialTemperature::typeName + "Type"
+      + "Eqn",
+        &TEqn()
+    );
+    
+}
+
+void Foam::regionTypes::conductNegPotentialTemperature::postSolve()
+{
+    // do nothing, add as required
+}
+
+void Foam::regionTypes::conductNegPotentialTemperature::solveRegion()
+{
+    // do nothing, add as required
+}
+
+void Foam::regionTypes::conductNegPotentialTemperature::prePredictor()
+{
+    // do nothing, add as required
+}
+
+void Foam::regionTypes::conductNegPotentialTemperature::momentumPredictor()
+{
+    // do nothing, add as required
+}
+
+void Foam::regionTypes::conductNegPotentialTemperature::pressureCorrector()
+{
+    // do nothing, add as required
+}
+
+void Foam::regionTypes::conductNegPotentialTemperature::meshMotionCorrector()
+{
+    // do nothing, add as required
+}
+
+// ************************************************************************* //
